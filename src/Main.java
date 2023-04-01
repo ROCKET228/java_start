d76403fe51255cf295e713a978070ed7e8143e64
ROCKET228 <michkahap@gmail.com>
2023-04-01 15:33:22 +0300

    Creat callculate array sum function

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-        func(1, 2);
+       int[] numbers = new int[] {5,6,8};
+       System.out.println(sum(numbers));
     }
 
-    public static void func(int first, int second){
-        int sum = first + second;
-        System.out.println("Answer: " + sum);
+    public static int sum(int[] num){
+        int sum = 0;
+
+        for(Integer i : num){
+            sum += i;
+        }
+
+        return sum;
     }
 }
