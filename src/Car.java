2fd4ae4c5be35cbb2b7204e3715b38dc6f9b26d7
ROCKET228 <michkahap@gmail.com>
2023-04-01 18:48:07 +0300

    Change class car

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,13 @@
 public class Car extends Transport{
+    public int length;
+    public Car(){}
+    public Car(int speed, int weight,int length, String color){
+       super(speed, weight, color);
+       this.length = length;
+    }
+
+    public void setValue(int speed, int weight,int length, String color){
+        super.setValue(speed, weight, color);
+        this.length = length;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-        Transport car = new Transport(100,100, "red");
+        Car car = new Car();
     }
 }
