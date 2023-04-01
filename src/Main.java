d2b9b208f7e74dd7b684debc80304f3469af8343
ROCKET228 <michkahap@gmail.com>
2023-04-01 17:55:13 +0300

    Creat class car

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,6 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-        Transport car = new Transport();
-        car.color = "red";
-        car.speed = 100;
-
-        Transport truck = new Transport();
-        truck.speed = 140;
-        truck.color = "Rd"
+        Transport car = new Transport(100,100, "red");
     }
 }
