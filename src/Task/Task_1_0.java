6f1526b9b9c54d6befcf0640bc06de1367b35fa7
ROCKET228 <michkahap@gmail.com>
2023-04-03 16:39:10 +0300

    Change file

---
+++ src/Task/Task_1_0.java
@@ -0,0 +1,15 @@
+package Task;
+
+public class Task_1_0 {
+    public static void main(String[] args) {
+        int n = 100;
+        double[] array = new double[n];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        int max = 0;
+        int min = 0;
+        int average = 0;
+    }
+}
