a6c7499a26687abbaefe5de809f374568999f45a
ROCKET228 <michkahap@gmail.com>
2023-04-04 13:25:14 +0300

    Create bubble sort

--- src/Task/bubble_sort.java
+++ src/Task/bubble_sort.java
@@ -1,4 +1,25 @@
 package Task;
 
 public class bubble_sort {
+    public static void main(String[] args) {
+        int n = 100;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length - i - 1; j++){
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
 }
