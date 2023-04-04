051f2e94ddbe3b5185342993ea1b8648bb264a90
ROCKET228 <michkahap@gmail.com>
2023-04-04 16:08:39 +0300

    End task

--- src/Task/remove_element_from_array.java
+++ src/Task/remove_element_from_array.java
@@ -1,4 +1,26 @@
 package Task;
+import java.util.Arrays;
 
 public class remove_element_from_array {
+    public static void main(String[] args) {
+        int test_array[] = {0,1,2,2,3,0,4,2};
+        System.out.println(Arrays.toString(removeElement(test_array, 2)));
+    }
+
+    private static int[] removeElement(int[] testArray, int target) {
+
+        int removeElementCount = 0;
+
+        for(int i = 0; i < testArray.length; i++){
+            if(testArray[i] == target){
+                removeElementCount++;
+            }else {
+                testArray[i - removeElementCount] = testArray[i];
+            }
+        }
+
+
+
+        return Arrays.copyOf(testArray, testArray.length - removeElementCount);
+    }
 }
