c246cd8426c186ed9ea8c485bd9c2a7fda900b8f
ROCKET228 <michkahap@gmail.com>
2023-04-05 15:21:56 +0300

    End task

--- src/Task/Cake.java
+++ src/Task/Cake.java
@@ -1,21 +1,22 @@
 package Task;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Cake {
     public static void main(String[] args) throws IOException {
         FileReader fileIn = new FileReader("input.txt");
         Scanner scanner = new Scanner(fileIn);
         String line = scanner.nextLine();
-        int number = Integer.parseInt(line);
+        int numberIn = Integer.parseInt(line);
+        int numberOut = 0;
+
+        numberOut = (numberIn == 1) ? 0 : (numberIn % 2 == 0) ? numberIn/2 : numberIn ;
 
 
         FileWriter fileOut = new FileWriter("output.txt");
-        fileOut.write(String.valueOf(number));
+        fileOut.write(String.valueOf(numberOut));
         fileOut.close();
         scanner.close();
     }
