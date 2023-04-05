8b0eb928df0da85e1c667a9438a3ede4b86695af
ROCKET228 <michkahap@gmail.com>
2023-04-05 16:35:43 +0300

    End task

--- src/Task/Coins.java
+++ src/Task/Coins.java
@@ -1,4 +1,40 @@
 package Task;
 
+import java.io.*;
+import java.util.*;
+
 public class Coins {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> coins = new ArrayList<>();
+        int one = 0;
+        int zero = 0;
+        int out;
+        String str;
+        FileReader file = new FileReader("input.txt"); // Считывание данных из файла
+        Scanner sc = new Scanner(file);
+        while(sc.hasNextLine()){
+            str = sc.nextLine();
+            coins.add(Integer.valueOf(str));
+        }
+        int coinsSize = coins.get(0);
+        coins.remove(0);
+
+        for (int i = 0; i < coinsSize; i++){
+            if(coins.get(i) == 1){
+                one++;
+            } else if (coins.get(i) == 0) {
+                zero++;
+            }
+        }
+
+        if(one >= zero){
+            out = zero;
+        }else{
+            out = one;
+        }
+
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(String.valueOf(out));
+        fileOut.close();
+    }
 }
