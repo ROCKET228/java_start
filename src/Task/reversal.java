d0e822f27ece14862e4f153276be854f25d99fe5
ROCKET228 <michkahap@gmail.com>
2023-04-05 16:44:42 +0300

    End task

--- src/Task/reversal.java
+++ src/Task/reversal.java
@@ -1,4 +1,31 @@
 package Task;
 
+import java.io.*;
+import java.util.*;
+
+
 public class reversal {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> numbers = new ArrayList<>();
+        StringBuilder out = new StringBuilder();
+        String str;
+        FileReader file = new FileReader("input.txt"); // Считывание данных из файла
+        Scanner sc = new Scanner(file);
+        str = sc.nextLine();
+        str = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        while(st.hasMoreTokens()){
+            numbers.add(st.nextToken());
+        }
+
+        Collections.reverse(numbers);
+
+        for(String number: numbers){
+            out.append(number).append(" ");
+        }
+
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(out.toString());
+        fileOut.close();
+    }
 }
