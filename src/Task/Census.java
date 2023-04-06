e19eb4bac8a7cb4fd36d7801ab9a114ee06bf323
ROCKET228 <michkahap@gmail.com>
2023-04-06 15:59:57 +0300

    End task

---
+++ src/Task/Census.java
@@ -0,0 +1,45 @@
+package Task;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Census {
+    private static ArrayList<Integer> data = new ArrayList<>();
+    private static int oldManNumber = -1;
+    private static int manAge = 0;
+
+    private static void dataValidation(){ //
+        int j = 0; // Счетчик номера жильца
+        for (int i = 0; i < data.size(); i++){
+            if(data.get(i) != 0 && i % 2 == 1){
+                j++;
+            }
+            if(i % 2 == 1 && data.get(i+1) == 1 && i+1 < data.size()){ // Проверка возраста мужчины
+                if(data.get(i) > manAge){
+                    manAge = data.get(i);
+                    oldManNumber = j;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        FileReader file = new FileReader("input.txt");
+        Scanner sc = new Scanner(file);
+        String str;
+        while (sc.hasNextLine()) {
+            str = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+            while (st.hasMoreTokens()) {
+                data.add(Integer.valueOf(st.nextToken()));
+            }
+        }
+        dataValidation();
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(String.valueOf(oldManNumber));
+        fileOut.close();
+    }
+}
