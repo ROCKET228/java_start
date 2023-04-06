13d7eeccc49030451f851525d221fa6cacc9079a
ROCKET228 <michkahap@gmail.com>
2023-04-06 14:52:01 +0300

    End task

--- src/Task/Phone.java
+++ src/Task/Phone.java
@@ -1,4 +1,41 @@
 package Task;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Phone {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int result = 0;
+        String str;
+        FileReader file = new FileReader("input.txt"); // Считывание данных из файла
+        Scanner sc = new Scanner(file);
+        str = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        while (st.hasMoreTokens()) {
+            numbers.add(Integer.valueOf(st.nextToken()));
+        }
+        int A = numbers.get(0);
+        int B = numbers.get(1);
+        int C = numbers.get(2);
+        int T = numbers.get(3);
+        
+        if(T <= A){
+            result = T * B;
+        } else if (T > A) {
+            int temp = T - A;
+            result = (A * B) + (temp * C);
+        }
+
+
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(String.valueOf(result));
+        fileOut.close();
+    }
 }
