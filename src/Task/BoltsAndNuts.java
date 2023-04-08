e929926c7b56238f25b8e4aee696020fad7bd64d
ROCKET228 <michkahap@gmail.com>
2023-04-08 16:28:56 +0300

    Change input txt

--- src/Task/BoltsAndNuts.java
+++ src/Task/BoltsAndNuts.java
@@ -1,4 +1,12 @@
 package Task;
 
+import java.io.*;
+import java.util.*;
+
 public class BoltsAndNuts {
+    public static void main(String[] args) throws IOException {
+        FileReader file = new FileReader("input.txt");
+        Scanner scanner = new Scanner(file);
+        String str = scanner.nextLine();
+    }
 }
