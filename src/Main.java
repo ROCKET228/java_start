4c6eb88d1dcbc837a4a1fb59657da0c53c0a21dd
ROCKET228 <michkahap@gmail.com>
2023-04-13 15:28:59 +0300

    Star study reduce stream

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int[] array = {5,1,3,6,6};
 
-        Arrays.stream(array).forEach(System.out::println);
 
     }
 }
