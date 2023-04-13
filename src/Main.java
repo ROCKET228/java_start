536a7b15037a6ec28ed7e0e9731e0d237677f7ad
ROCKET228 <michkahap@gmail.com>
2023-04-13 15:21:07 +0300

    Start study foreach stream

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,6 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(2);
-        numbers.add(5);
-        numbers.add(89);
-        numbers.add(4);
-        numbers.add(12);
 
-        List<Integer> list = numbers.stream().filter(integer -> integer.intValue() > 4).collect(Collectors.toList());
-
-        System.out.println(list);
     }
 }
