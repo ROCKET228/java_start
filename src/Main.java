0d27b8f528370f4cc7e9899556c183683c03eb1c
ROCKET228 <michkahap@gmail.com>
2023-04-14 17:28:07 +0300

    Start task №409 from leetcode

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,5 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Stream<Integer> stream1 = Stream.of(1,23,1,65,8,1,87);
-        System.out.println(stream1.distinct().peek(System.out::println).count());
     }
 }
