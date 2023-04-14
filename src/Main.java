6d1180155880f3a953b509296a5386c87fa46c98
ROCKET228 <michkahap@gmail.com>
2023-04-14 15:44:14 +0300

    Start study peek stream

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Stream<Integer> stream1 = Stream.of(1,23,54,65,8,34,87);
-        System.out.println(stream1.distinct().count());
+
     }
 }
