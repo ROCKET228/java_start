b29c05da32fd7933c061746101885b25efaffcdd
ROCKET228 <michkahap@gmail.com>
2023-04-14 15:26:28 +0300

    Start study chaining stream

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int[] array = {3,8,1,5,7,812,43,6,54};
-        array = Arrays.stream(array).sorted().toArray();
-        System.out.println(Arrays.toString(array));
+
     }
 }
