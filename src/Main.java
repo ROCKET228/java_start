887f6619cf96585b75225b0b4bebd3d54bb02549
ROCKET228 <michkahap@gmail.com>
2023-04-14 15:30:34 +0300

    End study chaining stream

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        int[] array = {3,8,1,5,7,812,43,6,54};
+        int result = Arrays.stream(array).filter(e -> e%2==1).map(e -> {if(e%3==0) {e=e/3;} return e;})
+                .reduce((a,e) -> a+e).getAsInt();
+
 
     }
 }
