b06873c0acd2581d9e8e0fb54c2c7fd6abb64162
ROCKET228 <michkahap@gmail.com>
2023-04-14 18:01:52 +0300

    End task №409 from leetcode

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,21 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println(longestPalindrome("Answer: "+"abccccdd"));
+    }
 
+    public static int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        System.out.println(count.toString());
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
     }
 }
