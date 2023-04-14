7e4368ee6ac54786385bc70e7b98afc21f708588
ROCKET228 <michkahap@gmail.com>
2023-04-14 15:41:03 +0300

    Start study distinct stream

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Stream<Integer> stream1 = Stream.of(1,23,54,65,8,34,87);
-        Stream<Integer> stream2= Stream.of(1,23,54,65,8,34,87);
-        Stream<Integer> stream3 = Stream.concat(stream1,stream2);
-        System.out.println(stream3);
+
     }
 }
