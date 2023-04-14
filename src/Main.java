2e9df162f4e97bc4ce324d38274aee7edf3c0776
ROCKET228 <michkahap@gmail.com>
2023-04-14 15:40:42 +0300

    End study contact stream

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-
+        Stream<Integer> stream1 = Stream.of(1,23,54,65,8,34,87);
+        Stream<Integer> stream2= Stream.of(1,23,54,65,8,34,87);
+        Stream<Integer> stream3 = Stream.concat(stream1,stream2);
+        System.out.println(stream3);
     }
 }
