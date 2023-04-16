19e0ee6f742de920f55548b2dedb001868d11c62
ROCKET228 <michkahap@gmail.com>
2023-04-16 19:05:28 +0300

    End study multithread

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args){
+       MyThread thread = new MyThread();
+       thread.start();
     }
+}
 
+class MyThread extends Thread{
+    public void run(){
+        for(int i = 0; i < 1000; i++){
+            System.out.println(i);
+        }
+    }
 }
