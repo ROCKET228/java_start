362e482fcbe092b35598e0f95cb1b1edbc03842b
ROCKET228 <michkahap@gmail.com>
2023-04-17 17:03:15 +0300

    End study ArrayBlockingQueue

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,50 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue<Integer> list = new ArrayBlockingQueue<>(4);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.offer(5);
 
+
+        Runnable runnable1 = () -> {
+            Iterator<Integer> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(iterator.next());
+            }
+        };
+
+        Runnable runnable2 = () -> {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                list.remove(3);
+                list.add(8);
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(list);
 
     }
 
