12be3cf688ec703fef138493b8f7c2d0a4790696
ROCKET228 <michkahap@gmail.com>
2023-04-17 16:54:59 +0300

    End study CopyOnWriteArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("Hi");
+        list.add("ok");
+        list.add("mu");
+        list.add("by");
+        list.add("js");
 
+
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(iterator.next());
+            }
+        };
+
+        Runnable runnable2 = () -> {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                list.remove(3);
+                list.add("Misha");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(list);
 
     }
 
