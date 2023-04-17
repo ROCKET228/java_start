b372a0ef607d90e21c146877f63fd02ac48d6e7e
ROCKET228 <michkahap@gmail.com>
2023-04-17 17:25:09 +0300

    End study Comparable

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-
+        List<String> list = new ArrayList<>();
+        list.add("Misha1");
+        list.add("Misha2");
+        list.add("Misha3");
 
     }
 
