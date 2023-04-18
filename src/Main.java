5e3a994af7b59c83b0ee7158ae3b24676cad5f4e
ROCKET228 <michkahap@gmail.com>
2023-04-18 17:21:43 +0300

    Start study Enumeration

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        List<String> list = new ArrayList<>();
-        list.add("Misha1");
-        list.add("Misha2");
-        list.add("Misha3");
 
     }
 
