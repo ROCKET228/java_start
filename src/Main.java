1bc44d468657cef3ece25e760f97b93097d2dff6
ROCKET228 <michkahap@gmail.com>
2023-05-11 17:43:39 +0300

    27. Remove Element

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@
 import java.util.*;
 
 public class Main {
-
+    public static int removeElement(int[] nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                System.out.println(nums[j]);
+                nums[j] = nums[i];
+                System.out.println(nums[j]);
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums) + j);
+        return j;
+    }
     public static void main(String[] args) throws InterruptedException {
-
-
+        int[] nums = {3,2,2,3};
+        removeElement(nums, 3);
     }
 
 }
